498c8f73a12387cfc7fa58e3af831b4901ab098d
Colleen O'Briant <cobriant@uoregon.edu>
2021-04-16 07:48:34 -0700

    load_id_data

--- R/load_data.R
+++ R/load_data.R
@@ -6,17 +6,11 @@ load_murder_data <- function(){
   get_fit_now_member <- read_csv("https://raw.githubusercontent.com/cobriant/dplyrmurdermystery/master/data/get_fit_now_member.csv")
   income <- read_csv("https://raw.githubusercontent.com/cobriant/dplyrmurdermystery/master/data/income.csv")
   interview <- read_csv("https://raw.githubusercontent.com/cobriant/dplyrmurdermystery/master/data/interview.csv")
-  people <- read_csv("https://raw.githubusercontent.com/cobriant/dplyrmurdermystery/master/data/person.csv")
-}
-
-load_person_data <- function(){
   person <- read_csv("https://raw.githubusercontent.com/cobriant/dplyrmurdermystery/master/data/person.csv")
 }
 
-load_income_data <- function(){
+load_id_data <- function(){
+  person <- read_csv("https://raw.githubusercontent.com/cobriant/dplyrmurdermystery/master/data/person.csv")
   income <- read_csv("https://raw.githubusercontent.com/cobriant/dplyrmurdermystery/master/data/income.csv")
-}
-
-load_license_data <- function(){
   drivers_license <- read_csv("https://raw.githubusercontent.com/cobriant/dplyrmurdermystery/master/data/drivers_license.csv")
 }
